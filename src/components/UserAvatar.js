0a16c2bc08424db2fff760411ade220ba972574b
Serban Stefancu <serban@Serbans-MacBook-Pro.local>
2020-05-01 19:26:53 +0200

    improved avatar component

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -3,18 +3,27 @@ import {View, StyleSheet} from 'react-native';
 import Avatar from 'react-native-user-avatar';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-
-const UserAvatar = () => {
+const UserAvatar = ({
+  starred = false,
+  name = 'Placeholder Name',
+  imagesrc = 'https://srv-file14.gofile.io/download/MCHVO5/download.png',
+}) => {
   return (
     <View style={styles.container}>
-      <Avatar
-        size={64}
-        name="Michelle Hunt"
-        src="https://srv-file14.gofile.io/download/MCHVO5/download.png"
-        style={{width: 69, height: 69, backgroundColor: 'rgba(235, 235, 235, 0.63)'}}
-      />
-      <View style={{width: 25, height: 25, borderRadius: 25/2, position: 'absolute', top: 0, right: 0, backgroundColor: '#4C94F5', elevation: 3, alignItems: 'center', justifyContent: 'center'}}>
-      <Icon name="star" size={20} color={'white'} />
+      <Avatar size={64} name={name} src={imagesrc} style={styles.avatar} />
+      <View
+        style={[
+          styles.starcontainer,
+          {
+            backgroundColor: starred ? '#4C94F5' : 'rgba(77, 149, 245, 0.58)',
+            elevation: starred ? 3 : 0,
+          },
+        ]}>
+        <Icon
+          name={starred ? 'star' : 'star-border'}
+          size={20}
+          color={'white'}
+        />
       </View>
     </View>
   );
@@ -24,10 +33,25 @@ export default UserAvatar;
 
 const styles = StyleSheet.create({
   container: {
-    height: 80,
-    width: 80,
+    height: 75,
+    width: 75,
     //backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  avatar: {
+    width: 69,
+    height: 69,
+    backgroundColor: 'rgba(235, 235, 235, 0.63)',
+  },
+  starcontainer: {
+    width: 25,
+    height: 25,
+    borderRadius: 25 / 2,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
