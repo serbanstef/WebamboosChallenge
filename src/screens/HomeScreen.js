a38f6c53c0cf430eb307446f9d783c468ca8a236
Serban Stefancu <serban@Serbans-MacBook-Pro.local>
2020-05-07 17:06:45 +0200

    change app's name to YourSkillSet

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WorkInProgress from '../components/WorkInProgress';
 import GradientStatusBar from '../components/GradientStatusBar';
+import { Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
